0dc1e930726996706710d41757e1b3890b863a21
Giovanna <giovannagmoeller@gmail.com>
2023-02-14 13:13:22 -0300

    aula 5.4 - mostrando o desempenho e organizando o código

--- iQuiz/iQuiz/ViewController/DesempenhoViewController.swift
+++ iQuiz/iQuiz/ViewController/DesempenhoViewController.swift
@@ -18,11 +18,20 @@ class DesempenhoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configurarLayout()
+        configurarDesempenho()
         // Do any additional setup after loading the view.
     }
     
     func configurarLayout() {
+        navigationItem.hidesBackButton = true
         botaoReiniciarQuiz.layer.cornerRadius = 12.0
+    }
+    
+    func configurarDesempenho() {
+        guard let pontuacao = pontuacao else { return }
+        resultadoLabel.text = "Você acertou \(pontuacao) de \(questoes.count) questões"
+        let percentual = (pontuacao * 100) / questoes.count
+        percentualLabel.text = "Percentual final: \(pontuacao)%"
     }
 
     /*
