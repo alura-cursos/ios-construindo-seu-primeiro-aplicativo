e0236da385c2af256cab90ee0b7db3fdef277b4d
Giovanna <giovannagmoeller@gmail.com>
2023-02-14 15:48:32 -0300

    extra: extensões e uma correção de 'pontuacao' para 'percentual'

---
+++ iQuiz/iQuiz/Extensões/UIColor+.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+.swift
+//  iQuiz
+//
+//  Created by Giovanna Moeller on 14/02/23.
+//
+
+import UIKit
+
+extension UIColor {
+    static var corDeFundoVerde = UIColor(red: 11/255, green: 161/255, blue: 53/255, alpha: 1.0)
+    static var corDeFundoVermelho = UIColor(red: 211/255, green: 17/255, blue: 17/255, alpha: 1.0)
+}
